e039affe29978cc31bbfe5d599821fe32e3b0e0f
RudoCoder <rudomakuwe@gmail.com>
2025-05-25 01:24:58 +0200

    basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#Assign specific values
+number1 = 10
+number2 = 5
+
+#perform calculations
+print("\n")
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#print the results
+print(f"Addition of {number1} and {number1} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
