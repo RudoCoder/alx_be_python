9c29607a6386005aa9d5f983ca7279b607ec908a
RudoCoder <rudomakuwe@gmail.com>
2025-06-08 22:31:25 +0200

    Update

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,9 +7,13 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        return num1 / num2
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+
     else:
-        return "Invalid Operation"
+        return "Error: Invalid operation"
 
 #Input user data
 print("Arithmetic Operations")
